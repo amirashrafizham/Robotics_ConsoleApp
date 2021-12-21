ce7afbbf8b7fc4ee621fb16fb08b0922ee816fb1
Amir Ashraf <amirashrafizham@gmail.com>
2021-12-21 20:38:44 +0800

    Formatted

--- Program.cs
+++ Program.cs
@@ -29,62 +29,62 @@ namespace ConsolePiTop
                     switch (input.Key)
                     {
                         case (ConsoleKey.W):
-                            sh.SetPixel(2,4,Color.Purple);
-                            sh.SetPixel(4,4,Color.Purple);
-                            sh.SetPixel(2,6,Color.Purple);
-                            sh.SetPixel(4,6,Color.Purple);
+                            sh.SetPixel(2, 4, Color.Purple);
+                            sh.SetPixel(4, 4, Color.Purple);
+                            sh.SetPixel(2, 6, Color.Purple);
+                            sh.SetPixel(4, 6, Color.Purple);
                             Console.WriteLine("Moving forward...");
                             Forward(fwSpeed);
                             Console.Clear();
                             break;
                         case (ConsoleKey.D):
-                            sh.SetPixel(2,4,Color.Purple);
-                            sh.SetPixel(4,4,Color.Blue);
-                            sh.SetPixel(2,6,Color.Purple);
-                            sh.SetPixel(4,6,Color.Blue);
+                            sh.SetPixel(2, 4, Color.Purple);
+                            sh.SetPixel(4, 4, Color.Blue);
+                            sh.SetPixel(2, 6, Color.Purple);
+                            sh.SetPixel(4, 6, Color.Blue);
                             Console.WriteLine("Turning clockwise...");
                             TurnRight(turnSpeed);
                             Console.Clear();
                             break;
                         case (ConsoleKey.A):
-                            sh.SetPixel(2,4,Color.Blue);
-                            sh.SetPixel(4,4,Color.Purple);
-                            sh.SetPixel(2,6,Color.Blue);
-                            sh.SetPixel(4,6,Color.Purple);
+                            sh.SetPixel(2, 4, Color.Blue);
+                            sh.SetPixel(4, 4, Color.Purple);
+                            sh.SetPixel(2, 6, Color.Blue);
+                            sh.SetPixel(4, 6, Color.Purple);
                             Console.WriteLine("Turning counter-clockwise...");
                             TurnLeft(turnSpeed);
                             Console.Clear();
                             break;
                         case (ConsoleKey.S):
-                            sh.SetPixel(2,4,Color.Yellow);
-                            sh.SetPixel(4,4,Color.Yellow);
-                            sh.SetPixel(2,6,Color.Yellow);
-                            sh.SetPixel(4,6,Color.Yellow);
+                            sh.SetPixel(2, 4, Color.Yellow);
+                            sh.SetPixel(4, 4, Color.Yellow);
+                            sh.SetPixel(2, 6, Color.Yellow);
+                            sh.SetPixel(4, 6, Color.Yellow);
                             Console.WriteLine("Reversing...");
                             Reverse(turnSpeed);
                             Console.Clear();
                             break;
                         case (ConsoleKey.Z):
-                            sh.SetPixel(2,4,Color.Yellow);
-                            sh.SetPixel(4,4,Color.Red);
-                            sh.SetPixel(2,6,Color.Yellow);
-                            sh.SetPixel(4,6,Color.Red);
+                            sh.SetPixel(2, 4, Color.Yellow);
+                            sh.SetPixel(4, 4, Color.Red);
+                            sh.SetPixel(2, 6, Color.Yellow);
+                            sh.SetPixel(4, 6, Color.Red);
                             Console.WriteLine("Reversing counter-clockwise...");
                             ReverseLeft(turnSpeed);
                             Console.Clear();
                             break;
                         case (ConsoleKey.C):
-                            sh.SetPixel(2,4,Color.Red);
-                            sh.SetPixel(4,4,Color.Yellow);
-                            sh.SetPixel(2,6,Color.Red);
-                            sh.SetPixel(4,6,Color.Yellow);
+                            sh.SetPixel(2, 4, Color.Red);
+                            sh.SetPixel(4, 4, Color.Yellow);
+                            sh.SetPixel(2, 6, Color.Red);
+                            sh.SetPixel(4, 6, Color.Yellow);
                             Console.WriteLine("Reversing clockwise...");
                             ReverseRight(turnSpeed);
                             Console.Clear();
                             break;
                         case (ConsoleKey.L):
-                            sh.SetPixel(2,7,Color.Green);
-                            sh.SetPixel(5,3,Color.Blue);
+                            sh.SetPixel(2, 7, Color.Green);
+                            sh.SetPixel(5, 3, Color.Blue);
                             Console.WriteLine("Getting Ultrasonic reading...");
                             GetUltraSonic();
                             break;
@@ -111,125 +111,125 @@ namespace ConsolePiTop
         static void Forward(Speed rpmSpeed)
         {
 
-                EncoderMotor mtrLeft;
-                EncoderMotor mtrRight;
-                var expansionPlate = PiTop4Board.Instance.GetOrCreateExpansionPlate();
+            EncoderMotor mtrLeft;
+            EncoderMotor mtrRight;
+            var expansionPlate = PiTop4Board.Instance.GetOrCreateExpansionPlate();
 
-                mtrLeft = expansionPlate.GetOrCreateEncoderMotor(EncoderMotorPort.M2); //Port M1 Default Forward
-                mtrRight = expansionPlate.GetOrCreateEncoderMotor(EncoderMotorPort.M3); //Port M2 Default Reverse
-                mtrRight.ForwardDirection = ForwardDirection.CounterClockwise;
-                using (mtrLeft)
-                {
-                    mtrLeft.SetSpeed(rpmSpeed);
-                }
-                using (mtrRight)
-                {
-                    mtrRight.SetSpeed(rpmSpeed);
-                }
-                Thread.Sleep(2000);
-                using (mtrLeft)
-                {
-                    mtrLeft.Stop();
-                }
-                using (mtrRight)
-                {
-                    mtrRight.Stop();
-                }
+            mtrLeft = expansionPlate.GetOrCreateEncoderMotor(EncoderMotorPort.M2); //Port M1 Default Forward
+            mtrRight = expansionPlate.GetOrCreateEncoderMotor(EncoderMotorPort.M3); //Port M2 Default Reverse
+            mtrRight.ForwardDirection = ForwardDirection.CounterClockwise;
+            using (mtrLeft)
+            {
+                mtrLeft.SetSpeed(rpmSpeed);
+            }
+            using (mtrRight)
+            {
+                mtrRight.SetSpeed(rpmSpeed);
+            }
+            Thread.Sleep(2000);
+            using (mtrLeft)
+            {
+                mtrLeft.Stop();
+            }
+            using (mtrRight)
+            {
+                mtrRight.Stop();
+            }
         }
 
         static void TurnRight(Speed rpmSpeed)
         {
 
-                EncoderMotor mtrLeft;
-                var expansionPlate = PiTop4Board.Instance.GetOrCreateExpansionPlate();
-                mtrLeft = expansionPlate.GetOrCreateEncoderMotor(EncoderMotorPort.M2); //Port M1 Default Forward
-                using (mtrLeft)
-                {
-                    mtrLeft.SetSpeed(rpmSpeed);
-                    Thread.Sleep(2000);
-                    mtrLeft.Stop();
-                }
+            EncoderMotor mtrLeft;
+            var expansionPlate = PiTop4Board.Instance.GetOrCreateExpansionPlate();
+            mtrLeft = expansionPlate.GetOrCreateEncoderMotor(EncoderMotorPort.M2); //Port M1 Default Forward
+            using (mtrLeft)
+            {
+                mtrLeft.SetSpeed(rpmSpeed);
+                Thread.Sleep(2000);
+                mtrLeft.Stop();
+            }
 
         }
 
         static void TurnLeft(Speed rpmSpeed)
         {
 
-                EncoderMotor mtrRight;
-                var expansionPlate = PiTop4Board.Instance.GetOrCreateExpansionPlate();
-                mtrRight = expansionPlate.GetOrCreateEncoderMotor(EncoderMotorPort.M3); //Port M2 Default Reverse
-                mtrRight.ForwardDirection = ForwardDirection.CounterClockwise;
+            EncoderMotor mtrRight;
+            var expansionPlate = PiTop4Board.Instance.GetOrCreateExpansionPlate();
+            mtrRight = expansionPlate.GetOrCreateEncoderMotor(EncoderMotorPort.M3); //Port M2 Default Reverse
+            mtrRight.ForwardDirection = ForwardDirection.CounterClockwise;
 
-                using (mtrRight)
-                {
-                    mtrRight.SetSpeed(rpmSpeed);
-                    Thread.Sleep(2000);
-                    mtrRight.Stop();
-                }
+            using (mtrRight)
+            {
+                mtrRight.SetSpeed(rpmSpeed);
+                Thread.Sleep(2000);
+                mtrRight.Stop();
+            }
         }
 
         static void Reverse(Speed rpmSpeed)
         {
 
-                EncoderMotor mtrLeft;
-                EncoderMotor mtrRight;
-                var expansionPlate = PiTop4Board.Instance.GetOrCreateExpansionPlate();
+            EncoderMotor mtrLeft;
+            EncoderMotor mtrRight;
+            var expansionPlate = PiTop4Board.Instance.GetOrCreateExpansionPlate();
 
 
-                mtrLeft = expansionPlate.GetOrCreateEncoderMotor(EncoderMotorPort.M2); //Port M1 Default Forward
-                mtrRight = expansionPlate.GetOrCreateEncoderMotor(EncoderMotorPort.M3); //Port M2 Default Reverse 
-                mtrLeft.ForwardDirection = ForwardDirection.CounterClockwise;
+            mtrLeft = expansionPlate.GetOrCreateEncoderMotor(EncoderMotorPort.M2); //Port M1 Default Forward
+            mtrRight = expansionPlate.GetOrCreateEncoderMotor(EncoderMotorPort.M3); //Port M2 Default Reverse 
+            mtrLeft.ForwardDirection = ForwardDirection.CounterClockwise;
 
-                using (mtrLeft)
-                {
-                    mtrLeft.SetSpeed(rpmSpeed);
-                }
-                using (mtrRight)
-                {
-                    mtrRight.SetSpeed(rpmSpeed);
-                }
-                Thread.Sleep(2000);
-                using (mtrLeft)
-                {
-                    mtrLeft.Stop();
-                }
-                using (mtrRight)
-                {
-                    mtrRight.Stop();
-                }
+            using (mtrLeft)
+            {
+                mtrLeft.SetSpeed(rpmSpeed);
+            }
+            using (mtrRight)
+            {
+                mtrRight.SetSpeed(rpmSpeed);
+            }
+            Thread.Sleep(2000);
+            using (mtrLeft)
+            {
+                mtrLeft.Stop();
+            }
+            using (mtrRight)
+            {
+                mtrRight.Stop();
+            }
         }
 
         static void ReverseLeft(Speed rpmSpeed)
         {
 
-                EncoderMotor mtrLeft;
-                var expansionPlate = PiTop4Board.Instance.GetOrCreateExpansionPlate();
+            EncoderMotor mtrLeft;
+            var expansionPlate = PiTop4Board.Instance.GetOrCreateExpansionPlate();
 
-                mtrLeft = expansionPlate.GetOrCreateEncoderMotor(EncoderMotorPort.M2); //Port M1 Default Forward
-                mtrLeft.ForwardDirection = ForwardDirection.CounterClockwise;
+            mtrLeft = expansionPlate.GetOrCreateEncoderMotor(EncoderMotorPort.M2); //Port M1 Default Forward
+            mtrLeft.ForwardDirection = ForwardDirection.CounterClockwise;
 
-                using (mtrLeft)
-                {
-                    mtrLeft.SetSpeed(rpmSpeed);
-                    Thread.Sleep(2000);
-                    mtrLeft.Stop();
-                }
+            using (mtrLeft)
+            {
+                mtrLeft.SetSpeed(rpmSpeed);
+                Thread.Sleep(2000);
+                mtrLeft.Stop();
+            }
         }
 
         static void ReverseRight(Speed rpmSpeed)
         {
 
-                EncoderMotor mtrRight;
-                var expansionPlate = PiTop4Board.Instance.GetOrCreateExpansionPlate();
+            EncoderMotor mtrRight;
+            var expansionPlate = PiTop4Board.Instance.GetOrCreateExpansionPlate();
 
-                mtrRight = expansionPlate.GetOrCreateEncoderMotor(EncoderMotorPort.M3); //Port M2 Default Reverse
+            mtrRight = expansionPlate.GetOrCreateEncoderMotor(EncoderMotorPort.M3); //Port M2 Default Reverse
 
-                using (mtrRight)
-                {
-                    mtrRight.SetSpeed(rpmSpeed);
-                    Thread.Sleep(2000);
-                    mtrRight.Stop();
-                }
+            using (mtrRight)
+            {
+                mtrRight.SetSpeed(rpmSpeed);
+                Thread.Sleep(2000);
+                mtrRight.Stop();
+            }
         }
 
         static void GetUltraSonic()
@@ -244,7 +244,7 @@ namespace ConsolePiTop
             }
             catch (System.Exception)
             {
-                Console.WriteLine("try again");   
+                Console.WriteLine("try again");
                 Console.WriteLine("  ");
             }
         }
@@ -256,8 +256,8 @@ namespace ConsolePiTop
             {
                 try
                 {
-                    sh.SetPixel(6,5,Color.Orange);
-                    sh.SetPixel(3,7,Color.Violet);
+                    sh.SetPixel(6, 5, Color.Orange);
+                    sh.SetPixel(3, 7, Color.Violet);
                     Console.WriteLine($"Temperature: Sensor1: {sh.Temperature.DegreesCelsius:N2} °C   Sensor2: {sh.Temperature2.DegreesCelsius:N2} °C");
                     Console.WriteLine($"Humidity: {sh.Humidity} %rH");
                     Console.WriteLine($"Pressure: {sh.Pressure} hPa");
@@ -265,7 +265,7 @@ namespace ConsolePiTop
                 }
                 catch (System.Exception)
                 {
-                    Console.WriteLine("try again");   
+                    Console.WriteLine("try again");
                     Console.WriteLine("  ");
                 }
             }
@@ -278,8 +278,8 @@ namespace ConsolePiTop
             {
                 try
                 {
-                    sh.SetPixel(2,4,Color.Blue);
-                    sh.SetPixel(4,4,Color.Red);
+                    sh.SetPixel(2, 4, Color.Blue);
+                    sh.SetPixel(4, 4, Color.Red);
                     Console.WriteLine($"Acceleration: {sh.Acceleration:N2} g");
                     Console.WriteLine($"Angular rate: {sh.AngularRate:N2} DPS");
                     Console.WriteLine($"Magnetic induction: {sh.MagneticInduction:N2} gauss");
@@ -287,7 +287,7 @@ namespace ConsolePiTop
                 }
                 catch (System.Exception)
                 {
-                    Console.WriteLine("try again");   
+                    Console.WriteLine("try again");
                     Console.WriteLine("  ");
                 }
             }

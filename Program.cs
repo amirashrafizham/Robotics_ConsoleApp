50a0a8883e365e9aa609a65cdab56fa092d805be
Amir Ashraf Izham <amirashrafizham@Amir-Ashrafs-Mac.local>
2022-05-28 19:52:53 +0800

    added battery integration

--- Program.cs
+++ Program.cs
@@ -12,3 +12,41 @@ using System.Threading.Tasks;
 using ConsolePiTop;
 
 
+
+
+static void CheckBattery()
+{
+    var batteryState = PiTop4Board.Instance.BatteryState.TimeRemaining.Minutes;
+    Console.WriteLine($"{batteryState}");
+}
+
+
+static void ServoHeadController()
+{
+
+    ServoMotor mtrHorizontal;
+    ServoMotor mtrVertical;
+
+    Console.Write("Set horizontal angle: ");
+    int valueHorizontal = Convert.ToInt16(Console.ReadLine());
+    Angle setAngleHorizontal = Angle.FromDegrees(valueHorizontal);
+
+
+    Console.Write("Set vertical angle: ");
+    int valueVertical = Convert.ToInt16(Console.ReadLine());
+    Angle setAngleVertical = Angle.FromDegrees(valueVertical);
+
+    var expansionPlate = PiTop4Board.Instance.GetOrCreateExpansionPlate();
+    mtrHorizontal = expansionPlate.GetOrCreateServoMotor(ServoMotorPort.S1); //Port M1 Default Forward
+    mtrVertical = expansionPlate.GetOrCreateServoMotor(ServoMotorPort.S4);
+
+    using (mtrHorizontal)
+    {
+        mtrHorizontal.GoToAngle(setAngleHorizontal);
+    }
+
+    using (mtrVertical)
+    {
+        mtrVertical.GoToAngle(setAngleVertical);
+    }
+}
